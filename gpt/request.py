0f1b0ee248291cf60928d70212b9ee9d44531f77
pengxuan <pengxuan@1token.trade>
2023-07-07 17:46:39 +0800

    other: remove redudant code

--- gpt/request.py
+++ gpt/request.py
@@ -1,14 +1,7 @@
+# 示例：
 # 输入：爬虫程序将交易所新的公告链接
 # 调用gpt总结，分析，返回结构化的结果
 # 参考 https://liaokong.gitbook.io/llm-kai-fa-jiao-cheng/#pa-qu-wang-ye-bing-shu-chu-json-shu-ju
-def Request():
-    print("")
-
-
-
-# 输入： 文本类型数据
-# 输出： 调用gpt总结，分析，返回结构化的结果
-# 用于测试展示时使用，可能交易所公告没有那么及时更新
 
 from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
