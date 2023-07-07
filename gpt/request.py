74bb035a681199d7a2c896f67456780a6fb247c6
pengxuan <pengxuan@1token.trade>
2023-07-07 10:18:18 +0800

    add: test

---
+++ gpt/request.py
@@ -0,0 +1,11 @@
+# 输入：爬虫程序将交易所新的公告链接
+# 调用gpt总结，分析，返回结构化的结果
+# 参考 https://liaokong.gitbook.io/llm-kai-fa-jiao-cheng/#pa-qu-wang-ye-bing-shu-chu-json-shu-ju
+def Request():
+    print("")
+
+
+
+# 输入： 文本类型数据
+# 输出： 调用gpt总结，分析，返回结构化的结果
+# 用于测试展示时使用，可能交易所公告没有那么及时更新
